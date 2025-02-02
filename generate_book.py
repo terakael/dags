eebdffbc109886e684a2f9c8de1bb10b3da5eb64
dan <terakael@gmail.com>
2025-02-02 18:32:57 +0900

    -

--- generate_book.py
+++ generate_book.py
@@ -1,6 +1,7 @@
 import base64
 from datetime import timedelta
 import json
+import re
 import time
 import os
 from typing import Dict, List
@@ -39,20 +40,24 @@ with DAG(
         
         Make sure the story contains a life lesson, and a humorous twist.  Keep the story positive.  Use simple words.
         
-        The output should be eight paragraphs long.
+        The story should be eight paragraphs long.
         
-        Your output should be a JSON object containing an array of eight paragraphs.
+        Your output should be a JSON object containing a fitting title, and an array of eight paragraphs.
         """
 
         conn = BaseHook.get_connection("gemini_api")
         client = genai.Client(api_key=conn.password)
+
+        class Story(BaseModel):
+            title: str
+            story: list[str]
 
         response = client.models.generate_content(
             model="gemini-2.0-flash-exp",
             contents=prompt,
             config=types.GenerateContentConfig(
                 response_mime_type="application/json",
-                response_schema={"type": "array", "items": {"type": "string"}},
+                response_schema=Story,
             ),
         )
 
@@ -226,8 +231,10 @@ with DAG(
         return {"image_prompt": image_prompt, "paragraph_text": paragraph_text}
 
     @task
-    def generate_image(prompt_data, dag_run, task_instance):
-        page_dir = f"/media/seagate/flask-static/book/static/{dag_run.conf['title']}/{task_instance.map_index}"
+    def generate_image(title, prompt_data, task_instance):
+        title_directory = re.sub(r"[^a-z_]", "", title.lower().replace(" ", "_"))
+
+        page_dir = f"/media/seagate/flask-static/book/static/{title_directory}/{task_instance.map_index}"
         os.makedirs(page_dir, exist_ok=True)
 
         conn = BaseHook.get_connection("openai_api")
@@ -249,15 +256,15 @@ with DAG(
             f.write(prompt_data["paragraph_text"])
 
     story = generate_story()
-    summary = understand_story(story)
-    character_descriptions = get_character_descriptions(story)
+    summary = understand_story(story["story"])
+    character_descriptions = get_character_descriptions(story["story"])
 
     paragraph_descriptions = get_paragraph_description.partial(
         summary=summary, characters=character_descriptions
-    ).expand(paragraph=story)
+    ).expand(paragraph=story["story"])
 
     prompt_data = generate_image_prompt.partial(
         characters=character_descriptions
     ).expand_kwargs(paragraph_descriptions)
 
-    generate_image.expand(prompt_data=prompt_data)
+    generate_image.partial(title=story["title"]).expand(prompt_data=prompt_data)
